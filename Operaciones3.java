8e3e581446fd3df8b6f80d5f05bccf43def4c2d2
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-05-10 23:35:19 -0600

    Create Operaciones3.java

---
+++ Operaciones3.java
@@ -0,0 +1,10 @@
+public class Operaciones3 {
+    public static void main(String[] args) {
+        double num1 = 2;
+    double num2 = 38;
+    double res = 0;
+    res = num1/num2;
+    System.out.println("El resultado de dividir "+num1+" entre "+ num2+" es = "+res);
+    }
+    
+}
